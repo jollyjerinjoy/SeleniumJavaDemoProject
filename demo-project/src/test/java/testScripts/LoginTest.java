d29400aad3ce32166927d0a6834f92290588ba79
jolly <jollyjerinjoy@gmail.com>
2025-12-17 08:54:58 +0000

    second commit

--- demo-project/src/test/java/testScripts/LoginTest.java
+++ demo-project/src/test/java/testScripts/LoginTest.java
@@ -18,7 +18,7 @@ public class LoginTest extends Base{
 			password.sendKeys(passwordvalue);
 		WebElement submit=driver.findElement(By.xpath("//button[@type='submit']"));
 			submit.click();
-			System.out.println("submit");
+			System.out.println("submit3");
 		
 			String nav=driver.getCurrentUrl();
 			Assert.assertEquals(nav, "https://groceryapp.uniqassosiates.com/admin");
