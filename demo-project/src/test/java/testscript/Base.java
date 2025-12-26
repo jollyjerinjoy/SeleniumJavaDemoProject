33bf1c54430322d751c9a6087642e24891dcac75
jolly <jollyjerinjoy@gmail.com>
2025-12-26 17:17:35 +0000

    Cross browser implementation

--- demo-project/src/test/java/testscript/Base.java
+++ demo-project/src/test/java/testscript/Base.java
@@ -2,16 +2,39 @@ package testscript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import com.beust.jcommander.Parameter;
 
 public class Base {
 	public WebDriver driver; //declaring globally so that every methods can use this
 	@BeforeMethod
-	public void browserInitialization() {
+	@Parameters("browser")  //Parameterization for multiple browser
+	public void browserInitialization(String browser) throws Exception {
+		if(browser.equalsIgnoreCase("chrome"))
 		//WebDriver driver=new ChromeDriver();
 		//Global – in framework, base class. After loading url. Since it si base class – applicable to all. Static wait.
-		driver=new ChromeDriver();
+		{
+			driver=new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("edge"))
+		{
+			driver=new EdgeDriver();
+		}
+		else
+		{
+			throw new Exception("Invalid browser");
+		}
+
+		
 		driver.get("https://groceryapp.uniqassosiates.com/admin");
 		//driver.get("https://demo.guru99.com/test/guru99home/");
 		driver.manage().window().maximize()	;
