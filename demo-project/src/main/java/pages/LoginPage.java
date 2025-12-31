981e978ac252b58088906c15f32f0509b13a6039
jolly <jollyjerinjoy@gmail.com>
2025-12-31 09:30:51 +0000

    Extent Report Utility

--- demo-project/src/main/java/pages/LoginPage.java
+++ demo-project/src/main/java/pages/LoginPage.java
@@ -1,9 +1,15 @@
 package pages;
 
+import java.awt.AWTException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import constant.Constant;
+import utilities.FileUploadUtility;
+import utilities.PageUtility;
 
 
 
@@ -11,7 +17,9 @@ public class LoginPage {
 //POM -page factory-login has two class page class and test class.
 //page factory used to locate WebWlements , used "finbBy
 //WebElements and methods	
-	    public WebDriver driver;
+	        public WebDriver driver;
+	        PageUtility pageutility= new PageUtility();
+	        FileUploadUtility fileuploadutility =new FileUploadUtility();
 		@FindBy(xpath="//input[@name='username']")WebElement username;
 		@FindBy(xpath="//input[@name='password']")WebElement password;
 		@FindBy(xpath="//button[@type='submit']")WebElement submit;
@@ -21,6 +29,9 @@ public LoginPage(WebDriver driver) {
 		this.driver=driver; //assign current class driver to global driver
 		PageFactory.initElements(driver, this);  //static method ,hence called classname.method <2 para, local driver, current class instance driver
 		//to initialize webelements we use initElements.
+		
+		
+		
 		}
 public void enterTheUsername(String user)
 {
@@ -45,4 +56,15 @@ public boolean isAlertDisplayed()
 			return alert.isDisplayed();
 
 }
+//public void countrydropdown()
+//{
+//	pageutility.selectByVisibleTextDropdownMethod(alert, "State"); //
+//}
+
+//public void fileupload() throws AWTException
+//{
+//	fileuploadutility.FileUploadusingRobotclass(alert,Constant.Apple);
+	
+//}
+
 }
