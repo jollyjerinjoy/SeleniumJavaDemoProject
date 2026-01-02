3c13054a9700f2e0d48402f85d5c917e632c0124
jolly <jollyjerinjoy@gmail.com>
2026-01-02 07:46:45 +0000

    mvnInstall,Screenshot utility, extend report

--- demo-project/src/test/java/testscript/Base.java
+++ demo-project/src/test/java/testscript/Base.java
@@ -1,14 +1,19 @@
 package testscript;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import com.beust.jcommander.Parameter;
+
+import utilities.ScreenshotUtility;
 
 public class Base {
 	public WebDriver driver; //declaring globally so that every methods can use this
@@ -42,15 +47,22 @@ public class Base {
 		//driver.quit();
 		}
 	@AfterMethod(alwaysRun=true)
-	public void browserClose()   {
+	public void browserQuit(ITestResult iTestResult) throws IOException {
+		if (iTestResult.getStatus() == ITestResult.FAILURE)
+		{
+			ScreenshotUtility scrShot = new ScreenshotUtility(); // creating obj
+			scrShot.getScreenShot(driver, iTestResult.getName());
+		}
+		driver.quit();
+	////public void browserClose()   {
 	//	driver.close(); //closes only the current page
-	driver.quit();//closes all the pages in the window   - use quit
-	}
+	////driver.quit();//closes all the pages in the window   - use quit
+	////}
 		
 	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	//	Base base=new Base();
 	//	base.browserInitialization();
 	//	}
-
+	}
 }
